2b37584c5eb69a15ab7b0fef99d5a81abd330600
Er-shivam11 <shivamnirmal.097@gmail.com>
2024-07-11 14:58:04 +0530

    added crud fully

--- ASPCoreWebAPI/ASPCoreWebAPI/Controllers/EmployeesController.cs
+++ ASPCoreWebAPI/ASPCoreWebAPI/Controllers/EmployeesController.cs
@@ -1,7 +1,10 @@
 ﻿using ASPCoreWebAPI.Data;
+using ASPCoreWebAPI.Models;
+using ASPCoreWebAPI.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 
 namespace ASPCoreWebAPI.Controllers
 {
@@ -24,6 +27,52 @@ namespace ASPCoreWebAPI.Controllers
         {
             var allEmpployees = dbContext.Employees.ToList();
             return Ok(allEmpployees);
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee(EmployeeDTO employeeDto)
+
+        {
+            var employee = new Employee
+            {
+                Name = employeeDto.Name,
+                Email = employeeDto.Email,
+                Phone = employeeDto.Phone,
+                Salary = employeeDto.Salary
+            };
+            dbContext.Employees.Add(employee);
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employeeid = dbContext.Employees.Find(id);
+            if (employeeid == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(employeeid);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployeeById(Guid id,UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employeeid = dbContext.Employees.Find(id);
+            if (employeeid == null)
+            {
+                return NotFound();
+
+            }
+            employeeid.Name = updateEmployeeDto.Name;
+            employeeid.Email = updateEmployeeDto.Email;
+            employeeid.Phone = updateEmployeeDto.Phone;
+            employeeid.Salary = updateEmployeeDto.Salary;
+            dbContext.SaveChanges();
+            return Ok(employeeid);
         }
     }
 }
